7195c61eed979daffe96c765b53cd5e6268b6034
GopinathJayakumar <GopinathJayakumar@GopinathJayakumar>
2020-01-16 02:38:37 +0530

    DropDown

--- src/main/java/firstDemo/FirstDemoClass.java
+++ src/main/java/firstDemo/FirstDemoClass.java
@@ -1,8 +1,6 @@
 package firstDemo;
 
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class FirstDemoClass {	
 	
@@ -26,35 +24,7 @@ public class FirstDemoClass {
 		driver.findElementById("password").sendKeys("crmsfa");
 		
 		// Click Login Button - (Element level)
-		driver.findElementByClassName("decorativeSubmit").click();
-		
-		// click crm.sfa link
-		driver.findElementByLinkText("CRM/SFA").click();
-		
-		//click create lead link
-		driver.findElementByLinkText("Create Lead").click();
-	
-		//Select class for Dropdown (how to handle with dropdown)
-		
-		// 1. identify the dropdown element nd pass to select class
-		
-		WebElement source = driver.findElementById("createLeadForm_dataSourceId");	
-		Select dd = new Select(source);					
-		
-		// 2. select specific value from that select dropdown element
-		
-		// choosing value from dropdown - 3 options
-	
-		dd.selectByVisibleText("Other");			// Way 1 
-		// dd.selectByValue("LEAD_COLDCALL");		// Way 2
-		// dd.selectByIndex(2);						// Way 3
-		
-		//close Browser
-		driver.close();
-		
-		
-		
-		
+		driver.findElementByClassName("decorativeSubmit").click();	
 		
 	}
 
